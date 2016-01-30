7a937d1af5f8d8d0241338f3dbdbf3c5837233e7
Adrian Smith <agnomen@gmail.com>
2016-01-29 23:35:44 -0500

    Clone javascript libraries from Blow-up

---
+++ src/audio.js
@@ -0,0 +1,81 @@
+var AUDIO = (function (baseURL) {
+    "use strict";
+
+    var gAudioContext = null,
+        gNoteOn = false;
+    try {
+        var Constructor = window.AudioContext || window.webkitAudioContext;
+        gAudioContext = new Constructor();
+    } catch (error) {
+        console.log("Error initializing audio:");
+        console.log(error);
+    }
+    
+    function audioNoteOn() {
+        if (!gNoteOn) {
+            gNoteOn = true;
+            if(gAudioContext !== null) {
+                // Trick to enable audio without downloading a sound from:
+                // https://paulbakaus.com/tutorials/html5/web-audio-on-ios/
+                // create empty buffer
+                var buffer = gAudioContext.createBuffer(1, 1, 22050);
+                var source = gAudioContext.createBufferSource();
+                source.buffer = buffer;
+
+                // connect to output (your speakers)
+                source.connect(gAudioContext.destination);
+
+                // play the file
+                source.noteOn(0);
+            }
+        }
+    }
+
+    function SoundEffect(resource) {
+        this.resource = resource;
+        this._source = null;
+        this._buffer = null;
+        
+        resource = baseURL + resource;
+        var self = this;
+        
+        if (gAudioContext !== null) {
+            var request = new XMLHttpRequest();
+            request.open("GET", resource, true);
+            request.responseType = "arraybuffer";
+            request.onload = function () {
+                var audioData = request.response;
+                gAudioContext.decodeAudioData(audioData,
+                    function (buffer) {
+                        self._buffer = buffer;
+                    },
+                    function (e) {
+                        console.log("Error with decoding audio data" + e.err);
+                    });
+            };
+            request.send();
+        }
+    }
+        
+    SoundEffect.prototype.isLoaded = function () {
+        return gAudioContext === null || this._buffer !== null;
+    };
+
+    SoundEffect.prototype.play = function () {
+        if (gAudioContext === null || this._buffer === null) {
+            return;
+        }
+        if (this._source) {
+            this._source.disconnect(gAudioContext.destination);
+        }
+        this._source = gAudioContext.createBufferSource();
+        this._source.buffer = this._buffer;
+        this._source.connect(gAudioContext.destination);
+        this._source.start();
+    };
+    
+    return {
+        SoundEffect: SoundEffect,
+        noteOn: audioNoteOn
+    };
+}(rootURL));
---
+++ src/flipbook.js
@@ -0,0 +1,45 @@
+﻿var Flipbook = (function () {
+    function Flipbook(imageBatch, baseName, frameCount, digits) {
+        this.frames = [];
+        for (var i = 1; i <= frameCount; ++i) {
+            var number = i.toString();
+            while (number.length < digits) {
+                number = "0" + number;
+            }
+            this.frames.push(imageBatch.load(baseName + number + ".png"));
+        }
+    }
+    
+    Flipbook.prototype.setupPlayback = function(frameTime, loop) {
+        return {
+            elapsed: 0,
+            timePerFrame: frameTime,
+            fractionComplete: 0,
+            loop: loop === true
+        };
+    };
+    
+    Flipbook.prototype.updatePlayback = function(elapsed, playback) {
+        var totalLength = playback.timePerFrame * this.frames.length;
+        playback.elapsed += elapsed;
+        if(playback.loop) {
+            playback.elapsed = playback.elapsed % totalLength;
+        }
+        if (playback.elapsed > totalLength) {
+            playback.fractionComplete = 0;
+            return true;
+        } else {
+            playback.fractionComplete = playback.elapsed / totalLength;
+            return false;
+        }
+    };
+    
+    Flipbook.prototype.draw = function(context, playback, location, width, height, center) {
+        var index = Math.min(this.frames.length - 1, Math.floor(playback.elapsed / playback.timePerFrame)),
+            x = location.x - (center ? width * 0.5 : 0),
+            y = location.y - (center ? height * 0.5 : 0);
+        context.drawImage(this.frames[index], x, y, width, height);
+    };
+    
+    return Flipbook;
+}());
